b4cb12f7831625cf504a7a14695d2fe0605bff6e
PecutKuda <73393693+PecutKuda@users.noreply.github.com>
2022-05-30 12:07:08 +0700

    Covinfo Part 1 - Elliot (PecutKuda)
    Sampai di durasi 1:31:24, jangan lupa baca note pada atas file.
    Dilanjutkan dengan fitur booking ambulance dan Covid-19 Medicine yang basis fiturnya sama dengan sebelumnya (buy dan add to cart)
    
    Diperhatikan nama variabel (cth: fruitDIV jadi hotSalesDIV), lakukan penyesuaian nama yaa untuk mengubah nama variabel

---
+++ Project Covinfo/main.js
@@ -0,0 +1,67 @@
+//GLOBAL
+var products=[];
+var carItems=[];
+var cart_n = document.getElementById('cart_n');
+// DIVS
+var hotSalesDIV = document.getElementById("hotSalesDIV");
+var bookingAmbulanceDIV = document.getElementById("bookingAmbulanceDIV");
+var covid19MedDIV = document.getElementById("covid19MedDIV");
+//INFORMATION
+var HOTSALES = [
+    {name: 'Panadol Forte Merah', price:12000}, 
+    {name: 'Panadol Forte Paracetamol', price:15000},
+    {name: 'Zadol Forte', price:12000}, 
+    {name: 'Betadine', price:15000},
+    {name: 'Salonpas', price:20000},
+    {name: 'Vitamin C', price:65000}
+];
+var BOOKINGAMBULANCE = [
+    {name: 'Rumah Sakit Selaguri', price:200000},
+    {name: 'Rumah Sakit Thamrin', price:180000},
+    {name: 'Rumah Sakit Medika', price: 0}
+];
+var COVID19MED = [
+    {name: 'Ivermectin', price:130000}, 
+    {name: 'Ivermectin 2', price:130000}, 
+    {name: 'Ivermectin 3', price:130000}, 
+    {name: 'Ivermectin', price:130000}
+];
+//HTML
+function HTMLhotSalesProduct(con) {
+    let URL = `Asset-code/hotSalesAsset/hotSales${con}.jpg`;
+    let btn = `btnHotSales${con}`;
+    return `
+        <div class ="col-md-4">
+            <div class = "card mb-4 shadow-sm">
+                <img class="card-img-top" style="height:16rem;" src="${URL}"    
+                alt = "Card image cap">  
+                <div class="card-body box-shadow-15">
+                    <i style = "color:orange;" class="fa fa-star"></i>
+                    <i style = "color:orange;" class="fa fa-star"></i>
+                    <i style = "color:orange;" class="fa fa-star"></i>
+                    <i style = "color:orange;" class="fa fa-star"></i>
+                    <p class="card-text">Name: ${HOTSALES[con-1].name}</p>
+                    <p class="card-text">Price: Rp. ${HOTSALES[con-1].price},00</p>
+                    <div class="d-flex justify-content-between align-items-center">
+                        <div class="btn-group">
+                            <button type-"button" onclick="cart2('${HOTSALES[con-1].name}', '${HOTSALES[con-1].price}', '${URL}', '${con}', '${btn}')"
+                            class="btn btn-sm btn-outline-secondary" >Buy</button>
+                            
+                            <button id="${btn}" type-"button" onclick="cart('${HOTSALES[con-1].name}', '${HOTSALES[con-1].price}', '${URL}', '${con}', '${btn}')"
+                            class="btn btn-sm btn-outline-secondary" >Add to cart</button>
+                        </div>
+                        <small class="text-muted">Free shipping</small>
+                    </div> 
+                </div>  
+            </div>
+        </div>
+    `
+}
+//RENDER
+function render() {
+    for (let index = 1; index <= 6 ; index++) {
+        hotSalesDIV.innerHTML+=`${HTMLhotSalesProduct(index)}`;
+    }
+}
+
+//di elliot 1:31:24
