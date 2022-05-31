9478fd5e1f9df53ace0c7f00a8a6144c07ac2832
CharlesKristov <charles.christopher@binus.ac.id>
2022-05-31 15:52:26 +0700

    feat: bookAmbulance Section

--- Project Covinfo/main.js
+++ Project Covinfo/main.js
@@ -16,9 +16,9 @@ var HOTSALES = [
     {name: 'Vitamin C', price:65000}
 ];
 var BOOKINGAMBULANCE = [
-    {name: 'Rumah Sakit Selaguri', price:200000},
-    {name: 'Rumah Sakit Thamrin', price:180000},
-    {name: 'Rumah Sakit Medika', price: 0}
+    {name: 'Rumah Sakit Awal Bros', price:200000},
+    {name: 'Rumah Sakit Primaya', price:180000},
+    {name: 'Rumah Sakit Siloam', price: 150000}
 ];
 var COVID19MED = [
     {name: 'Ivermectin', price:130000}, 
@@ -57,6 +57,38 @@ function HTMLhotSalesProduct(con) {
         </div>
     `
 }
+
+// function HTMLhotSalesProduct(con) {
+//     let URL = `Asset-code/bookAmbulanceAsset/RS${con}.jpg`;
+//     let btn = `btnRS${con}`;
+//     return `
+//         <div class ="col-md-4">
+//             <div class = "card mb-4 shadow-sm">
+//                 <img class="card-img-top" style="height:16rem;" src="${URL}"    
+//                 alt = "Card image cap">  
+//                 <div class="card-body box-shadow-15">
+//                     <i style = "color:orange;" class="fa fa-star"></i>
+//                     <i style = "color:orange;" class="fa fa-star"></i>
+//                     <i style = "color:orange;" class="fa fa-star"></i>
+//                     <i style = "color:orange;" class="fa fa-star"></i>
+//                     <p class="card-text">Name: ${RS[con-1].name}</p>
+//                     <p class="card-text">Price: Rp. ${RS[con-1].price},00</p>
+//                     <div class="d-flex justify-content-between align-items-center">
+//                         <div class="btn-group">
+//                             <button type-"button" onclick="cart2('${RS[con-1].name}', '${RS[con-1].price}', '${URL}', '${con}', '${btn}')"
+//                             class="btn btn-sm btn-outline-secondary" >Buy</button>
+                            
+//                             <button id="${btn}" type-"button" onclick="cart('${RS[con-1].name}', '${RS[con-1].price}', '${URL}', '${con}', '${btn}')"
+//                             class="btn btn-sm btn-outline-secondary" >Add to cart</button>
+//                         </div>
+//                         <small class="text-muted">Free shipping</small>
+//                     </div> 
+//                 </div>  
+//             </div>
+//         </div>
+//     `
+// }
+
 //RENDER
 function render() {
     for (let index = 1; index <= 6 ; index++) {
